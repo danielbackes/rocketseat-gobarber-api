ea2c0145c27076988e28d87ec0b841091b4c47a5
Daniel Backes <danielbackes@protonmail.com>
2021-02-04 19:25:57 -0400

    fix(migrations): :bug: Create uuid-ossp extension that provide the uuid_generate_v4 database function

--- src/shared/infra/typeorm/migrations/1602640844455-CreateUserTokens.ts
+++ src/shared/infra/typeorm/migrations/1602640844455-CreateUserTokens.ts
@@ -3,6 +3,8 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 export default class CreateUserTokens1602640844455
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+
     await queryRunner.createTable(
       new Table({
         name: 'user_tokens',
